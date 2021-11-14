8085bdadb1755dc0a6986993f89da240f496ae72
JiwonDev <sylvester7412@gmail.com>
2021-11-14 17:05:38 +0900

    Init project setting - checkstyle 추가 - editorconfig 추가 - spotbug 추가

---
+++ src/main/java/kr/flab/movieon/MovieOnApplication.java
@@ -0,0 +1,13 @@
+package kr.flab.movieon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/flab/movieon/MovieOnApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.flab.movieon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
