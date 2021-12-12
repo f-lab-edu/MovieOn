40ae8c9ea4073768c58c6e9db27b88aa085230f9
Rebwon <msolo021015@naver.com>
2021-12-12 16:27:44 +0900

    Refactor multi module project
    - Reconfiguring each domain module into a sub-module of layered
      architecture format.

---
+++ account-api/account-presentation/src/main/java/kr/flab/movieon/account/presentation/TokenAuthController.java
@@ -0,0 +1,52 @@
+package kr.flab.movieon.account.presentation;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import kr.flab.movieon.account.application.AccountFacade;
+import kr.flab.movieon.account.infrastructure.security.domain.Token;
+import kr.flab.movieon.account.presentation.payload.AccountTokenResponse;
+import kr.flab.movieon.account.presentation.payload.LoginAccountCommand;
+import kr.flab.movieon.account.presentation.payload.RegisterAccountCommand;
+import kr.flab.movieon.account.presentation.payload.RegisterAccountResponse;
+import kr.flab.movieon.common.ApiResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping(path = "/api/auth",
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE)
+public class TokenAuthController {
+
+    private final AccountFacade accountFacade;
+
+    public TokenAuthController(AccountFacade accountFacade) {
+        this.accountFacade = accountFacade;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<RegisterAccountResponse>> register(
+        @Valid @RequestBody RegisterAccountCommand request) {
+        return null;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<AccountTokenResponse>> authenticate(
+        @Valid @RequestBody LoginAccountCommand request) {
+        return null;
+    }
+
+    @GetMapping("/refresh")
+    public Token regenerateToken(HttpServletRequest request) {
+        return null;
+        //return accountFacade.refresh(request.getHeader("Authorization"));
+    }
+
+}
---
+++ purchase-api/purchase-domain/src/main/java/kr/flab/movieon/purchase/domain/PurchaseFactory.java
@@ -0,0 +1,20 @@
+package kr.flab.movieon.purchase.domain;
+
+import java.math.BigDecimal;
+import kr.flab.movieon.purchase.domain.Purchase.PurchaseType;
+
+public final class PurchaseFactory {
+
+    private PurchaseFactory() {
+    }
+
+    public static Purchase pending(Long purchaserId, Long productId, String title,
+        BigDecimal price, int availableDays, String type) {
+        var purchasedProduct = PurchasedProduct.create(
+            productId, title,
+            price, availableDays);
+        var purchaser = new Purchaser(purchaserId);
+        return new Purchase(purchaser, purchasedProduct,
+            PurchaseType.valueOf(type));
+    }
+}
