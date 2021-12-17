2be47bd958d131f4c5870dd3dcfaeba5ec0a0de7
Rebwon <msolo021015@naver.com>
2021-12-17 12:18:36 +0900

    Refactor Notification Domain
    - removal hibernate
    - introduce NotificationSender

---
+++ notification-api/notification-infrastructure/src/main/java/kr/flab/movieon/notification/infrastructure/EmailNotificationSender.java
@@ -0,0 +1,18 @@
+package kr.flab.movieon.notification.infrastructure;
+
+import kr.flab.movieon.notification.domain.EmailNotification;
+import kr.flab.movieon.notification.domain.NotificationSender;
+
+public final class EmailNotificationSender implements
+    NotificationSender<EmailNotification> {
+
+    @Override
+    public Class<EmailNotification> appliesTo() {
+        return EmailNotification.class;
+    }
+
+    @Override
+    public void send(EmailNotification notification) {
+
+    }
+}
