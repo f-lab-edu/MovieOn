fea787bb509eedf6d729cfb0bf61de578e25f185
Rebwon <msolo021015@naver.com>
2022-01-12 18:48:31 +0900

    Add OrderCanceledEvent

--- order-api/order-domain/src/main/java/kr/flab/movieon/order/domain/Order.java
+++ order-api/order-domain/src/main/java/kr/flab/movieon/order/domain/Order.java
@@ -21,29 +21,42 @@ public class Order extends AbstractAggregateRoot {
 
     private List<OrderProduct> products;
 
+    private BigDecimal discountPrice;
     private LocalDateTime orderedAt;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 
     private Order(Customer customer, String payMethod, OrderStatus status, OrderType type,
-        List<OrderProduct> products) {
+        List<OrderProduct> products, BigDecimal discountPrice) {
         this.customer = customer;
         this.payMethod = payMethod;
         this.status = status;
         this.type = type;
         this.products = products;
+        this.discountPrice = discountPrice;
         registerEvent(new OrderCreatedEvent(this));
     }
 
     public static Order create(Customer customer, String payMethod,
-        OrderType type, List<OrderProduct> products) {
-        return new Order(customer, payMethod, OrderStatus.CREATED, type, products);
+        OrderType type, List<OrderProduct> products, BigDecimal discountPrice) {
+        return new Order(customer, payMethod, OrderStatus.CREATED, type, products, discountPrice);
     }
 
     public BigDecimal calculateTotalPrice() {
         return this.products.stream()
             .map(OrderProduct::getPrice)
-            .reduce(BigDecimal.ZERO, BigDecimal::add);
+            .reduce(BigDecimal.ZERO, BigDecimal::add)
+            .subtract(this.discountPrice);
+    }
+
+    public void complete() {
+        this.status = OrderStatus.COMPLETED;
+        registerEvent(new OrderCompletedEvent(this));
+    }
+
+    public void cancel() {
+        this.status = OrderStatus.CANCELED;
+        registerEvent(new OrderCanceledEvent(this));
     }
 
     public enum OrderType {
@@ -52,7 +65,6 @@ public class Order extends AbstractAggregateRoot {
 
     public enum OrderStatus {
         CREATED("주문 생성됨"),
-        PAYED("주문 결제됨"),
         COMPLETED("주문 완료됨"),
         CANCELED("주문 취소됨");
 
