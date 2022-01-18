9fd428dbf2babf0be01311beac2db6e3d13591e0
Rebwon <msolo021015@naver.com>
2022-01-18 20:43:52 +0900

    Order entity using surrogate key UUID

--- order-api/order-domain/src/main/java/kr/flab/movieon/order/domain/OrderCanceledEvent.java
+++ order-api/order-domain/src/main/java/kr/flab/movieon/order/domain/OrderCanceledEvent.java
@@ -3,16 +3,18 @@ package kr.flab.movieon.order.domain;
 import java.math.BigDecimal;
 import java.util.Date;
 import kr.flab.movieon.common.domain.model.DomainEvent;
+import lombok.Getter;
 
+@Getter
 public final class OrderCanceledEvent implements DomainEvent {
 
-    private final Long orderId;
+    private final String orderId;
     private final Long accountId;
     private final Date occurredOn;
     private final BigDecimal useOfPoint;
 
     public OrderCanceledEvent(Order order) {
-        this.orderId = order.getId();
+        this.orderId = order.getOrderId();
         this.accountId = order.getCustomer().getAccountId();
         this.occurredOn = new Date();
         this.useOfPoint = order.getUseOfPoint();
