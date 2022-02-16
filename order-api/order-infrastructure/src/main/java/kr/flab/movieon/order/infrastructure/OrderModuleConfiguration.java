23a3dab7f4d6bb65f672999fae189ba2eb7a0b68
Rebwon <msolo021015@naver.com>
2022-02-16 16:32:57 +0900

    Integrate Order Module

---
+++ order-api/order-infrastructure/src/main/java/kr/flab/movieon/order/infrastructure/OrderModuleConfiguration.java
@@ -0,0 +1,19 @@
+package kr.flab.movieon.order.infrastructure;
+
+import kr.flab.movieon.order.domain.OrderCommandHandler;
+import kr.flab.movieon.order.domain.OrderValidator;
+import kr.flab.movieon.order.domain.PaymentCompletedProcessor;
+import kr.flab.movieon.order.infrastructure.jpa.OrderRepositoryAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Import(value = {
+    OrderRepositoryAdapter.class,
+    OrderCommandHandler.class,
+    OrderValidator.class,
+    PaymentCompletedProcessor.class
+})
+@Configuration
+public class OrderModuleConfiguration {
+
+}
