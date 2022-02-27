221ec0fa8f1e9e6fb48e307bd21d7abde6e3a847
Rebwon <msolo021015@naver.com>
2022-02-27 20:24:00 +0900

    Add Transaction in OrderEventHandler

--- order-api/order-domain/src/main/java/kr/flab/movieon/order/domain/OrderCompletedEvent.java
+++ order-api/order-domain/src/main/java/kr/flab/movieon/order/domain/OrderCompletedEvent.java
@@ -11,14 +11,14 @@ public final class OrderCompletedEvent implements DomainEvent {
     private final String orderId;
     private final Long customerId;
     private final LocalDateTime orderedAt;
-    private final List<Long> products;
+    private final List<Long> items;
     private final Date occurredOn;
 
     public OrderCompletedEvent(Order order) {
         this.orderId = order.getOrderId();
         this.customerId = order.getCustomer().getAccountId();
         this.orderedAt = order.getCompletedAt();
-        this.products = order.getLineItems().stream()
+        this.items = order.getLineItems().stream()
             .map(OrderLineItem::getItemId)
             .collect(Collectors.toList());
         this.occurredOn = new Date();
@@ -41,7 +41,7 @@ public final class OrderCompletedEvent implements DomainEvent {
         return orderedAt;
     }
 
-    public List<Long> getProducts() {
-        return products;
+    public List<Long> getItems() {
+        return items;
     }
 }
--- order-api/order-infrastructure/src/main/java/kr/flab/movieon/order/infrastructure/OrderEventHandler.java
+++ order-api/order-infrastructure/src/main/java/kr/flab/movieon/order/infrastructure/OrderEventHandler.java
@@ -1,27 +1,30 @@
 package kr.flab.movieon.order.infrastructure;
 
-import kr.flab.movieon.order.domain.Order;
 import kr.flab.movieon.order.domain.PaymentCompletedEvent;
 import kr.flab.movieon.order.domain.PaymentCompletedProcessor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Component
-public class OrderEventHandler {
+public final class OrderEventHandler {
 
     private final PaymentCompletedProcessor processor;
     private final ApplicationEventPublisher publisher;
+    private final TransactionTemplate transactionTemplate;
 
     public OrderEventHandler(PaymentCompletedProcessor processor,
-        ApplicationEventPublisher publisher) {
+        ApplicationEventPublisher publisher,
+        TransactionTemplate transactionTemplate) {
         this.processor = processor;
         this.publisher = publisher;
+        this.transactionTemplate = transactionTemplate;
     }
 
     @EventListener
     public void handle(PaymentCompletedEvent event) {
-        Order order = processor.payed(event);
+        var order = transactionTemplate.execute(status -> processor.payed(event));
         order.pollAllEvents().forEach(publisher::publishEvent);
     }
 }
