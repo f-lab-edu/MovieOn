275655834663b66bbdc294c0b7eddaf06ac789f5
Rebwon <msolo021015@gmail.com>
2023-12-05 20:48:48 +0900

    Add CreateOrderUsecase Class

--- order-api/order-presentation/src/main/java/kr/flab/movieon/order/presentation/OrderApi.java
+++ order-api/order-presentation/src/main/java/kr/flab/movieon/order/presentation/OrderApi.java
@@ -3,7 +3,7 @@ package kr.flab.movieon.order.presentation;
 import java.net.URI;
 import kr.flab.movieon.common.AuthenticatedUser;
 import kr.flab.movieon.common.result.ApiResponseEnvelop;
-import kr.flab.movieon.order.application.OrderCommandExecutor;
+import kr.flab.movieon.order.application.CreateOrderUsecase;
 import kr.flab.movieon.order.presentation.request.CreateOrderRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public final class OrderApi implements OrderSpecification {
 
-    private final OrderCommandExecutor orderCommandExecutor;
+    private final CreateOrderUsecase createOrderUsecase;
     private final String baseUrl;
 
-    public OrderApi(OrderCommandExecutor orderCommandExecutor,
+    public OrderApi(CreateOrderUsecase createOrderUsecase,
                     @Value("${app.url}") String baseUrl) {
-        this.orderCommandExecutor = orderCommandExecutor;
+        this.createOrderUsecase = createOrderUsecase;
         this.baseUrl = baseUrl;
     }
 
     @Override
     public ResponseEntity<ApiResponseEnvelop<URI>> create(CreateOrderRequest request,
         AuthenticatedUser user) {
-        var info = orderCommandExecutor.create(user.id(), request.toCommand());
+        var info = createOrderUsecase.create(user.id(), request.toCommand());
         URI uri = URI.create(baseUrl + "/api/v1/orders/" + info);
         return ResponseEntity.ok(ApiResponseEnvelop.success(uri));
     }
