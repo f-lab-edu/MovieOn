acb5bc5528e3c6d65da6bb94a4501764afc00b1e
Jiwondev <sylvester7412@gmail.com>
2024-01-04 00:24:23 +0900

    fix/#131: local embeddedRedis가 실행되지 않더라도 빌드가 성공하도록 수정

--- app/src/main/java/kr/flab/movieon/supports/EmbeddedRedisConfiguration.java
+++ app/src/main/java/kr/flab/movieon/supports/EmbeddedRedisConfiguration.java
@@ -2,15 +2,20 @@ package kr.flab.movieon.supports;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
+import java.io.IOException;
+
 @Profile({"test", "local"})
 @Configuration
 public class EmbeddedRedisConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(EmbeddedRedisConfiguration.class);
 
     @Value("${spring.redis.port}")
     private int redisPort;
@@ -18,15 +23,26 @@ public class EmbeddedRedisConfiguration {
     private RedisServer redisServer;
 
     @PostConstruct
-    public void redisServer() throws IOException {
-        redisServer = RedisServer.builder()
-                .port(redisPort)
-                .build();
-        redisServer.start();
+    public void start() throws IOException {
+        if (ProcessUtil.isRunningPort(redisPort)) {
+            log.warn("Local embedded redis port is already used. Redis server did not start (port: {})", redisPort);
+            return;
+        }
+
+        try {
+            redisServer = RedisServer.builder()
+                    .port(redisPort)
+                    .build();
+            redisServer.start();
+            log.info("Embedded Redis Server enabled http://localhost:{}", redisPort);
+        } catch (Exception ex) {
+            log.error("Failed to start Embedded Redis Server http://localhost:{}", redisPort, ex);
+        }
+
     }
 
     @PreDestroy
-    public void stopRedis() {
+    public void stop() {
         if (redisServer != null) {
             redisServer.stop();
         }
