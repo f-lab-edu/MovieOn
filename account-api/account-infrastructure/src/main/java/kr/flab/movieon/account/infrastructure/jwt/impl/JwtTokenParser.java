4ef469ac546e56719986214060b4d9ac0ecb1ed0
Jiwondev <sylvester7412@gmail.com>
2024-01-04 00:23:41 +0900

    fix/#131: deprecated jwt 코드 제거

--- account-api/account-infrastructure/src/main/java/kr/flab/movieon/account/infrastructure/jwt/impl/JwtTokenParser.java
+++ account-api/account-infrastructure/src/main/java/kr/flab/movieon/account/infrastructure/jwt/impl/JwtTokenParser.java
@@ -1,15 +1,7 @@
 package kr.flab.movieon.account.infrastructure.jwt.impl;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.util.List;
 import kr.flab.movieon.account.infrastructure.jwt.AlreadyTokenExpiredException;
 import kr.flab.movieon.account.infrastructure.jwt.RawToken;
 import kr.flab.movieon.account.infrastructure.jwt.TokenParser;
@@ -17,6 +9,10 @@ import kr.flab.movieon.account.infrastructure.jwt.TokenProperties;
 import kr.flab.movieon.common.error.InvalidTokenException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public final class JwtTokenParser implements TokenParser {
 
@@ -31,17 +27,18 @@ public final class JwtTokenParser implements TokenParser {
     @Override
     public RawToken parse(String token) {
         try {
-            Jws<Claims> jwt = Jwts.parserBuilder()
-                .setSigningKey(getKey(tokenProperties.getBase64TokenSigningKey()))
-                .build()
-                .parseClaimsJws(token);
+            Jws<Claims> jwt = Jwts.parser()
+                    .verifyWith(getKey(tokenProperties.getBase64TokenSigningKey()))
+                    .build()
+                    .parseSignedClaims(token);
+
             String jti = (String) jwt.getBody().get("jti");
             if (jti == null) {
-                return new RawToken((String) jwt.getBody().get("email"),
-                    (List<String>) jwt.getBody().get("scopes"));
+                return new RawToken((String) jwt.getPayload().get("email"),
+                        (List<String>) jwt.getPayload().get("scopes"));
             }
             return new RawToken((String) jwt.getBody().get("email"), jti,
-                (List<String>) jwt.getBody().get("scopes"));
+                    (List<String>) jwt.getPayload().get("scopes"));
         } catch (UnsupportedJwtException | MalformedJwtException | IllegalArgumentException ex) {
             log.error("Invalid JWT Token", ex);
             throw new InvalidTokenException();
@@ -51,7 +48,7 @@ public final class JwtTokenParser implements TokenParser {
         }
     }
 
-    private Key getKey(String signKey) {
+    private SecretKey getKey(String signKey) {
         return Keys.hmacShaKeyFor(signKey.getBytes(StandardCharsets.UTF_8));
     }
 }
