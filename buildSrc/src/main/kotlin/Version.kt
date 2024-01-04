c87a60b75889537e8e5d820f1890fd15f325bf7a
jiwondev <sylvester7412@gmail.com>
2024-01-04 14:18:59 +0900

    build/#131 최상위 build.gradle version 분리

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -6,4 +6,9 @@ object Version {
     const val embeddedRedis = "0.7.3"
     const val fixtureMonkey = "0.3.5"
     const val mybatisSpring = "3.0.2"
+    const val kassava = "2.0.0"
+    const val logstashLogbackEncoder = "7.2"
+    const val kotest = "5.5.4"
+    const val kotestSpring = "1.1.2"
+    const val mockk = "1.12.0"
 }
